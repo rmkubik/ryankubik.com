1ceb6b585b4e1e87574ecbcb7075f16217df8a78
Ryan Kubik <rmkubik@mix.wvu.edu>
2016-11-04 17:59:27 -0400

    nabber links properly aligned and text of links hidden on xs

--- navbar.php
+++ navbar.php
@@ -13,19 +13,19 @@ if (!isset($home_active))
     <ul class="nav navbar-nav navbar-right">
       <li><a href="https://law5guy.itch.io">
         <object class="icon" data="/assets/images/icons/itchio.svg" type="image/svg+xml"></object>
-        Itch.io
+        <h5 class="hidden-xs">Itch.io</h5>
       </a></li>
       <li><a href="https://github.com/rmkubik">
         <object class="icon" data="/assets/images/icons/github.svg" type="image/svg+xml"></object>
-        Github
+        <h5 class="hidden-xs">Github</h5>
       </a></li>
       <li><a href="https://www.linkedin.com/in/ryankubik">
         <object class="icon" data="/assets/images/icons/linkedin.svg" type="image/svg+xml"></object>
-        LinkedIn
+        <h5 class="hidden-xs">LinkedIn</h5>
       </a></li>
       <li><a href="https://twitter.com/ryrykubes">
         <object class="icon" data="/assets/images/icons/twitter.svg" type="image/svg+xml"></object>
-        Twitter
+        <h5 class="hidden-xs">Twitter</h5>
       </a></li>
     </ul>
   </div>
