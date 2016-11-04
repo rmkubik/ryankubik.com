63ff5837476f1f1a838990d8c33707e7e9cd98e2
Ryan Kubik <rmkubik@mix.wvu.edu>
2016-11-04 01:31:04 -0400

    added svg icons for social media, they don't change on hover yet

--- navbar.php
+++ navbar.php
@@ -11,10 +11,10 @@ if (!isset($home_active))
       </a>
     </div>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="https://law5guy.itch.io">Itch.io</a></li>
-      <li><a href="https://github.com/rmkubik">Github</a></li>
-      <li><a href="https://www.linkedin.com/in/ryankubik">LinkedIn</a></li>
-      <li><a href="https://twitter.com/ryrykubes">Twitter</a></li>
+      <li><a href="https://law5guy.itch.io"><img class="icon" src="assets/images/icons/itchio.svg"/>Itch.io</a></li>
+      <li><a href="https://github.com/rmkubik"><img class="icon" src="assets/images/icons/github.svg"/>Github</a></li>
+      <li><a href="https://www.linkedin.com/in/ryankubik"><img class="icon" src="assets/images/icons/linkedin.svg"/>LinkedIn</a></li>
+      <li><a href="https://twitter.com/ryrykubes"><img class="icon" src="assets/images/icons/twitter.svg"/>Twitter</a></li>
     </ul>
   </div>
 </nav>
