06f5bc6781e19c98ce3d24b69ab903f91ea4e843
Ryan Kubik <rmkubik@mix.wvu.edu>
2016-11-07 13:11:58 -0500

    svg icons now hoverable AND clickable!!!!

--- navbar.php
+++ navbar.php
@@ -11,19 +11,19 @@ if (!isset($home_active))
       </a>
     </div>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="https://law5guy.itch.io">
+      <li><a class="svg" href="https://law5guy.itch.io">
         <object class="icon" data="/assets/images/icons/itchio.svg" type="image/svg+xml"></object>
         <h5 class="hidden-xs">Itch.io</h5>
       </a></li>
-      <li><a href="https://github.com/rmkubik">
+      <li><a class="svg" href="https://github.com/rmkubik">
         <object class="icon" data="/assets/images/icons/github.svg" type="image/svg+xml"></object>
         <h5 class="hidden-xs">Github</h5>
       </a></li>
-      <li><a href="https://www.linkedin.com/in/ryankubik">
+      <li><a class="svg" href="https://www.linkedin.com/in/ryankubik">
         <object class="icon" data="/assets/images/icons/linkedin.svg" type="image/svg+xml"></object>
         <h5 class="hidden-xs">LinkedIn</h5>
       </a></li>
-      <li><a href="https://twitter.com/ryrykubes">
+      <li><a class="svg" href="https://twitter.com/ryrykubes">
         <object class="icon" data="/assets/images/icons/twitter.svg" type="image/svg+xml"></object>
         <h5 class="hidden-xs">Twitter</h5>
       </a></li>
