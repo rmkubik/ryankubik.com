ecac75c6d2373770402a54f84ddd6023fdb314ba
Ryan Kubik <rmkubik@mix.wvu.edu>
2017-01-16 21:11:25 -0800

    added some faviconage

--- header.php
+++ header.php
@@ -4,6 +4,10 @@ if (!isset($title))
   $title = "";
 ?>
 
+<link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicons/rubik57.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicons/rubik72.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicons/rubik114.png" />
+<link rel="apple-touch-icon" sizes="144x144" href="assets/images/favicons/rubik144.png" />
 <link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="/css/style.css">
 <script src="/js/jquery-1.11.3.min.js" type="text/javascript"></script>
